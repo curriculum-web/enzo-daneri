21ab90006895e0a509d5061e37b6a3177dfbee64
Enzo Daneri <enzodaneri@hotmail.com>
2020-06-04 09:48:15 -0300

    Updates

--- build/bundle.js
+++ build/bundle.js
@@ -471,11 +471,26 @@ var app = (function () {
         }
     }
 
+    function cubicInOut(t) {
+        return t < 0.5 ? 4.0 * t * t * t : 0.5 * Math.pow(2.0 * t - 2.0, 3.0) + 1.0;
+    }
     function cubicOut(t) {
         const f = t - 1.0;
         return f * f * f + 1.0;
     }
 
+    function blur(node, { delay = 0, duration = 400, easing = cubicInOut, amount = 5, opacity = 0 }) {
+        const style = getComputedStyle(node);
+        const target_opacity = +style.opacity;
+        const f = style.filter === 'none' ? '' : style.filter;
+        const od = target_opacity * (1 - opacity);
+        return {
+            delay,
+            duration,
+            easing,
+            css: (_t, u) => `opacity: ${target_opacity - (od * u)}; filter: ${f} blur(${u * amount}px);`
+        };
+    }
     function fade(node, { delay = 0, duration = 400, easing = identity }) {
         const o = +getComputedStyle(node).opacity;
         return {
@@ -1459,6 +1474,7 @@ var app = (function () {
     function create_if_block_6(ctx) {
     	let div5;
     	let div0;
+    	let div0_intro;
     	let t0;
     	let h1;
     	let t3;
@@ -1562,6 +1578,13 @@ var app = (function () {
     		},
     		p: noop,
     		i(local) {
+    			if (!div0_intro) {
+    				add_render_callback(() => {
+    					div0_intro = create_in_transition(div0, blur, {});
+    					div0_intro.start();
+    				});
+    			}
+
     			if (!div5_intro) {
     				add_render_callback(() => {
     					div5_intro = create_in_transition(div5, fade, {});
